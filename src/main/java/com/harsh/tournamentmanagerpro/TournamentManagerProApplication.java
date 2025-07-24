2e57579249640e43ca160c45b04efef467ad52b0
harshrandhir258 <harshrandhir258@gmail.com>
2025-07-24 22:42:11 +0530

    Initial commit

---
+++ src/main/java/com/harsh/tournamentmanagerpro/TournamentManagerProApplication.java
@@ -0,0 +1,13 @@
+package com.harsh.tournamentmanagerpro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TournamentManagerProApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TournamentManagerProApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/harsh/tournamentmanagerpro/TournamentManagerProApplicationTests.java
@@ -0,0 +1,13 @@
+package com.harsh.tournamentmanagerpro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TournamentManagerProApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
